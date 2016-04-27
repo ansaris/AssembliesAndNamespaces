81485e8afe355ad174f284993714e6b0bf3e0849
ansaris <salman.ansari.usa@gmail.com>
2016-04-27 12:05:24 -0500

    Add example to write one string to a text file.

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic; //Not being used, which is why it is grayed out.
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace AssembliesAndNamespaces
 {
@@ -10,6 +10,16 @@ namespace AssembliesAndNamespaces
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello world!"); // System is not needed since namespace System is already used.
+            Console.ReadLine();
+
+            // bing search: site:microsoft.com write to a text file c#, get his from msdn.microsoft.com
+            // Example #2: Write one string to a text file.
+            string text = "A class is the most powerful data type in C#. Like a structure, " +
+                           "a class defines the data and behavior of the data type. ";
+            // WriteAllText creates a file, writes the specified string to the file,
+            // and then closes the file.    You do NOT need to call Flush() or Close().
+            System.IO.File.WriteAllText(@"C:\Users\Public\TestFolder\WriteText.txt", text);
         }
     }
 }
